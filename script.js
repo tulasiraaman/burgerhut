61a86c84eecfb681a6ebe2adae8a5e8b6c692838
tulasiraaman <tulasi28125@gmail.com>
2025-02-22 18:43:42 +0530

    commit message

---
+++ script.js
@@ -0,0 +1,21 @@
+var sidnav = document.querySelector(".side-navbar")
+
+function showNavbar() {
+    sidnav.style.left = "0"
+}
+
+function closeNavbar() {
+    sidnav.style.left = "-60%"
+}
+
+function contact() {
+    alert("Thank you. Our team will Message you soon...")
+}
+
+function btn(event) {
+    alert("This is not available now..")
+}
+
+function reservation() {
+    alert("Thank you. Your table was reserved...")
+}
